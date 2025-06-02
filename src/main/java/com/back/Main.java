3f8309925046832d6c78e4a0eecaa99634db9153
dbfgml20 <dbfgml20@gmail.com>
2025-06-02 10:33:21 +0900

    lab 1-3

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,13 +8,16 @@ public class Main {
     }
 
     private static void lab1() {
-        String input = "등록";
+        String input = "등록\n나의 죽음을 적들에게 알리지 말라.";
 
         Scanner scanner = new Scanner(input);
 
         String cmd = scanner.nextLine();
 
-        System.out.println(cmd);
+        String content = scanner.nextLine();
+
+        System.out.println("cmd : " + cmd);
+        System.out.println("content : " + content);
 
     }
 }
