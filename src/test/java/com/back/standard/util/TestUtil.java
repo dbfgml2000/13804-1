fd8e0b0702dddc48441403346a6b9a27f47a2da4
dbfgml20 <dbfgml20@gmail.com>
2025-06-02 11:22:05 +0900

    step 2, green

--- src/test/java/com/back/standard/util/TestUtil.java
+++ src/test/java/com/back/standard/util/TestUtil.java
@@ -1,6 +1,9 @@
 package com.back.standard.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.UncheckedIOException;
 import java.util.Scanner;
 
 public class TestUtil {private static final PrintStream ORIGINAL_OUT = System.out;
@@ -9,4 +12,18 @@ public class TestUtil {private static final PrintStream ORIGINAL_OUT = System.ou
         return new Scanner(input);
     }
 
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output, true));
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(ORIGINAL_OUT);
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
