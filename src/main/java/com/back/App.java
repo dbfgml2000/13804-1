23d103dcf85fdc881f2c1cc1e7d22760e870dd4b
dbfgml20 <dbfgml20@gmail.com>
2025-06-02 12:25:40 +0900

    step 7, green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,11 +1,14 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
     private int lastId = 0;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
 
     public App(Scanner scanner) {
         this.scanner = scanner;
@@ -26,9 +29,20 @@ public class App {
                     System.out.println("작가 : ");
                     String author = scanner.nextLine();
 
-                    lastId++;
+                    int id = ++lastId;
 
-                    System.out.println(lastId + "번 명언이 등록되었습니다.");
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for(WiseSaying wiseSaying : wiseSayings.reversed()){
+                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
+                    }
                 }
                 case "종료" -> {
                     return;
