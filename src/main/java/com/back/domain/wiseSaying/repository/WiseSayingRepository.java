8820fbb5d11b4eada6798c621b250bf94dc83c1e
dbfgml20 <dbfgml20@gmail.com>
2025-06-02 15:39:03 +0900

    step 8, green

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -4,6 +4,7 @@ import com.back.domain.wiseSaying.entity.WiseSaying;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class WiseSayingRepository {
     private int lastId = 0;
@@ -18,5 +19,23 @@ public class WiseSayingRepository {
 
     public List<WiseSaying> findForList() {
         return wiseSayings.reversed();
+    }
+
+    public int findIndexById(int id) {
+        return IntStream
+                .range(0, wiseSayings.size())
+                .filter(index -> wiseSayings.get(index).getId() == id)
+                .findFirst()
+                .orElse(-1);
+    }
+
+    public WiseSaying findById(int id) {
+        int index = findIndexById(id);
+        if(index == -1) return null;
+        return wiseSayings.get(index);
+    }
+
+    public void delete(WiseSaying wiseSaying) {
+        wiseSayings.remove(wiseSaying);
     }
 }
